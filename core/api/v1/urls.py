c3441d5ad361dc25aa759a395f257b179b895d57
emht <emht@users.noreply.github.com>
2023-03-15 16:30:02 +0530

    Add snippet application for code syntax highlighting Add pygment in project dependencies Update Url, settings.py Add model and serializers for snippets Add views for snippets Add migration for snippets

--- core/api/v1/urls.py
+++ core/api/v1/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 
 from . import views
+from snippets import views as snipped_views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -9,5 +10,7 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('snippets/', snipped_views.snippet_list),
+    path('snippets/<int:pk>/', snipped_views.snippet_detail),
     # url(r'', include('core.api.v1.urls', namespace='default')),
 ]
