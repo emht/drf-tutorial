4dd8a7a78dcccf2936b99c53d67fcbf6d5b4728f
emht <emht@users.noreply.github.com>
2023-03-15 16:49:23 +0530

    Using api_view for better request, response management Using format to allow multiple content type

--- core/api/v1/urls.py
+++ core/api/v1/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 
-from . import views
-from snippets import views as snipped_views
+from . import views as api_views
+from snippets import views as snippet_views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'users', api_views.UserViewSet)
+router.register(r'groups', api_views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('snippets/', snipped_views.snippet_list),
-    path('snippets/<int:pk>/', snipped_views.snippet_detail),
-    # url(r'', include('core.api.v1.urls', namespace='default')),
+    path('snippets/', snippet_views.snippet_list),
+    path('snippets/<int:pk>/', snippet_views.snippet_detail),
 ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns) # Doesn't work with routers in urlpattersn. Use query in url as ?format=json meanwhile
